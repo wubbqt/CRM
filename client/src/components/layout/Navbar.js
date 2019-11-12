5d94d22df8dfd8659781e9e82f6c5c746e9b0c8d
Michael <michael.zaccaria1@gmail.com>
2019-11-12 11:30:42 -0800

    Initial Commit

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,73 @@
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
+
+const Navbar = ({ title, icon }) => {
+    const authContext = useContext(AuthContext);
+    const contactContext = useContext(ContactContext);
+
+    const { isAuthenticated, logout, user } = authContext;
+    const { clearContacts } = contactContext;
+
+    const onLogout = () => {
+        logout();
+        clearContacts();
+    };
+
+    const authLinks = (
+        <Fragment>
+            <li className="ml-1">
+                <Link to="/">Home</Link>
+            </li>
+            <li className="ml-1">
+                <Link to="/about">About</Link>
+            </li>
+            <li li className="ml-1">
+                Hello, {user && user.name}!
+            </li>
+            <li className="ml-1">
+                <a onClick={onLogout} href="#1">
+                    <i className="fas fa-sign-out-alt "></i>
+                    <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+
+    const guestLinks = (
+        <Fragment>
+            <li className="ml-1">
+                <Link to="/about">About</Link>
+            </li>
+            <li li className="ml-1">
+                <Link to="/register">Create Account</Link>
+            </li>
+            <li li className="ml-1">
+                <Link to="/login">Login</Link>
+            </li>
+        </Fragment>
+    );
+
+    return (
+        <div className="navbar">
+            <h2>
+                <i className={icon} />
+                <Link to="/">{title}</Link>
+            </h2>
+            <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+        </div>
+    );
+};
+
+Navbar.protoTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+    title: "Contact Keeper"
+};
+
+export default Navbar;
